74a054894c3aea9050469f17f8f2d8507e7067a5
baruch <baruch@29f9d7e1-bebc-0310-b7c6-d7abf8ca9c08>
2004-06-09 18:32:33 +0000

    Automatic reconfigure upon configure script change

--- autogen.sh
+++ autogen.sh
@@ -11,10 +11,13 @@ aclocal -I m4
 echo "Update autoheader"
 autoheader
 
-#echo "Update Automake"
-#automake --verbose --foreign --add-missing
+echo "Update automake"
+set +e
+automake --foreign --add-missing 2>/dev/null
+set -e
 
 echo "Update autoconf"
 autoconf
 
+echo timestamp > stamp-h.in
 exit 0
